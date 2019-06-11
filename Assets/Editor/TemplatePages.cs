1ee91c5826239cabb03a6a71162c271e6c8de605
reprahkcin <bignickharper@gmail.com>
2019-06-11 16:19:21 -0700

    Canvas and Panel Generators

--- Assets/Editor/TemplatePages.cs
+++ Assets/Editor/TemplatePages.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class TemplatePages : EditorWindow
 {
+    
+    private GameObject new_canvas;
+
     public Color bg_color = new Color32(75,75,75,255);
     private Color text_color = new Color32(255,255,255,255);
     private Color nav_button_color = new Color32(215, 63, 9, 255);
@@ -24,11 +28,20 @@ public class TemplatePages : EditorWindow
         GetWindow<TemplatePages>("Page Generator");
     }
 
+
+
     void OnGUI()
     {
+        
         GUILayout.Label("Page Reference Dimensions:", EditorStyles.boldLabel);
         width = EditorGUILayout.IntField("Width", width);
         height = EditorGUILayout.IntField("Height", height);
+        if (GUILayout.Button("Generate Canvas"))
+        {
+            GenerateCanvas();
+        }
+
+
         GUILayout.Label("Set Colors:", EditorStyles.boldLabel);
         bg_color = EditorGUILayout.ColorField("Background Color", bg_color);
         text_color = EditorGUILayout.ColorField("Text Color", text_color);
@@ -41,9 +54,20 @@ public class TemplatePages : EditorWindow
         body_text = EditorGUILayout.TextField("Body Text", "");
         img = EditorGUILayout.ObjectField("Image", img, typeof(Sprite), true) as Sprite;
 
-        if (GUILayout.Button("Generate Canvas"))
+
+
+        if (GUILayout.Button("Generate Panel"))
         {
-            GenerateCanvas();
+            GeneratePanel();
+        }
+        if (GUILayout.Button("Panel Cleanup"))
+        {
+            if (new_canvas != null)
+            {
+                Navigation nav = new_canvas.GetComponent<Navigation>();
+                nav.ListCleanup();
+            }
+            
         }
 
     }
@@ -51,18 +75,39 @@ public class TemplatePages : EditorWindow
     void GenerateCanvas()
     {
         // Create a Canvas
-        GameObject newCanvas = new GameObject("Canvas");
-        Canvas c = newCanvas.AddComponent<Canvas>();
+        new_canvas = new GameObject("Canvas");
+        Canvas c = new_canvas.AddComponent<Canvas>();
         c.renderMode = RenderMode.ScreenSpaceOverlay;
-
         // Add the standard components to the new canvas
-        newCanvas.AddComponent<CanvasScaler>();
-        newCanvas.GetComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-        newCanvas.GetComponent<CanvasScaler>().referenceResolution = new Vector2(width, height);
-        newCanvas.AddComponent<GraphicRaycaster>();
+        new_canvas.AddComponent<CanvasScaler>();
+        new_canvas.GetComponent<CanvasScaler>().uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        new_canvas.GetComponent<CanvasScaler>().referenceResolution = new Vector2(width, height);
+        new_canvas.AddComponent<GraphicRaycaster>();
+        new_canvas.AddComponent<Navigation>();
+        // Add the event system
+        GameObject events = new GameObject("EventSystem");
+        events.AddComponent<EventSystem>();
+        events.AddComponent<StandaloneInputModule>();
 
+        // Generate Opening Slide
+        GameObject opening_panel = new GameObject("Panel0");
+        opening_panel.AddComponent<CanvasRenderer>();
+        opening_panel.AddComponent<RectTransform>();
+        Image i = opening_panel.AddComponent<Image>();
+        i.color = bg_color;
+        opening_panel.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        Navigation nav = new_canvas.GetComponent<Navigation>();
+        nav.panels = new List<GameObject>();
+        nav.AddToPanels(opening_panel);
+        opening_panel.transform.SetParent(new_canvas.transform,false);
+    }
+
+    void GeneratePanel()
+    {
         // Create a Panel
-        GameObject panel = new GameObject("Panel");
+        Navigation nav = new_canvas.GetComponent<Navigation>();
+        GameObject panel = new GameObject("Panel" + nav.panels.Count);
+              
         // Add the standard components to the new panel
         panel.AddComponent<CanvasRenderer>();
         panel.AddComponent<RectTransform>();
@@ -70,7 +115,9 @@ public class TemplatePages : EditorWindow
         i.color = bg_color;
         panel.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
         // Add the panel as a child of the new canvas
-        panel.transform.SetParent(newCanvas.transform, false);
+        panel.transform.SetParent(new_canvas.transform, false);
+        nav.AddToPanels(panel);
+
 
         // Create Button
         GameObject next_button = new GameObject("Next Button");
@@ -81,16 +128,21 @@ public class TemplatePages : EditorWindow
         next_button_text.GetComponent<TextMeshProUGUI>().text = "Next";
         next_button_text.GetComponent<TextMeshProUGUI>().font = header_font;
         next_button_text.transform.SetParent(next_button.transform, false);
+        next_button.AddComponent<RectTransform>();
+        next_button.GetComponent<RectTransform>().sizeDelta = new Vector2(160, 30);
         next_button.transform.SetParent(panel.transform, false);
         next_button.AddComponent<CanvasRenderer>();
         Image i_next = next_button.AddComponent<Image>();
         i_next.color = nav_button_color;
+        i_next.type = Image.Type.Sliced;
+        i_next.fillCenter = true;
         next_button.AddComponent<Button>();
 
 
 
-
-
+        
+        
     }
+        
 }
 
