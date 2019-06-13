5ce4bb0675dc1ef7956383041c83c0196f317a4d
Nick Harper <bignickharper@gmail.com>
2019-06-13 10:42:08 -0700

    staff meeting work

---
+++ Assets/Editor/ButtonGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ButtonGenerator : EditorWindow
+{
+    public GameObject prefab;
+
+    [MenuItem("Window/OSU Button Generator")]
+    public static void ShowWindow()
+    {
+        GetWindow<ButtonGenerator>("Button Generator");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Test");
+        if (GUILayout.Button("Instance Cube"))
+        {
+            var cube = Instantiate(prefab);
+        }
+    }
+}
